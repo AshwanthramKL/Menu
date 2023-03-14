c592666225ce7e15de0a62f6868372bfaa70a4ca
“[Martialeagle <k.l.ashwanthram@gmail.com>
2023-03-14 18:47:32 +0530

    added buttons dynamically

--- app.js
+++ app.js
@@ -71,16 +71,39 @@ const menu = [
     img: "./images/item-9.jpeg",
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
+  {
+    id: 10,
+    title: "steak dinner",
+    category: "dinner",
+    price: 39.99,
+    img: "./images/item-10.jpeg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
 ];
 
 
 const sectionCenter = document.querySelector('.section-center');
-const filterBtns = document.querySelectorAll('.filter-btn');
-
+const btnContainer = document.querySelector('.btn-container');
 // Load Items
 window.addEventListener('DOMContentLoaded', () =>{
     displayMenuItems(menu);
-})
+    const categories = menu.reduce((values, item)=>{
+      if(!values.includes(item.category)){
+        values.push(item.category);
+      }
+      return values;
+    }, ['all']);
+
+    const categoryBtns = categories.map((category)=>{
+      return `
+      <button class="filter-btn" type="button" data-id=${category}>${category}</button>
+      `
+    }).join("");
+    console.log(categoryBtns);
+    btnContainer.innerHTML = categoryBtns;
+  })
+
+const filterBtns = document.querySelectorAll('.filter-btn');
 
 // Filter Items
 filterBtns.forEach((btn)=>{
